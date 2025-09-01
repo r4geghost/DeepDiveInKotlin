ee59b0f1dca85ad9f4e86c823812b4a4865261d2
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-01 17:15:14 +0300

    4.15 Nothing - домашнее задание - условие

---
+++ src/homework/Nothing.kt
@@ -0,0 +1,32 @@
+package homework
+
+/**
+ * Класс NumberValidator выполняет проверку чисел.
+ */
+object NumberValidator {
+
+    /**
+     * Завершает выполнение программы или выбрасывает исключение с заданным сообщением.
+     * Возвращает тип Nothing.
+     */
+    fun terminate(message: String): Nothing {
+        // TODO: Реализуйте метод
+    }
+
+    /**
+     * Проверяет число на корректность.
+     * Если число некорректно, вызывает terminate.
+     * @return то же число, если данные корректны.
+     */
+    fun validate(number: Int): Int {
+        // TODO: Реализуйте метод
+    }
+}
+
+/**
+ * Функция обработки числа.
+ * Проверяет число с помощью NumberValidator.validate и выводит результат.
+ */
+fun processNumber(number: Int) {
+    // TODO: Реализуйте функцию
+}
