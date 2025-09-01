12ef8ed5de8b755c1891eeb89226bd8e9400d0a7
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-01 17:14:35 +0300

    4.15 Nothing

--- src/moduleThreeOOP/corporation/Corporation.kt
+++ src/moduleThreeOOP/corporation/Corporation.kt
@@ -1,19 +1,15 @@
 package moduleThreeOOP.corporation
 
 fun main() {
-    val director = WorkersRepository.findDirector()
-    director?.printInfo()
-    val assistant = WorkersRepository.findAssistant()
-    assistant?.printInfo()
-
-    val directorSalary = director?.salary ?: 0 // если левая часть == null, то = 0
-    val assistantSalary = assistant?.salary ?: 0
-    val sum = assistantSalary + directorSalary
-    println(sum)
-
-    val a: Unit = method() // Unit class -
-    // The type with only one value: the Unit object. This type corresponds to the void type in Java.
-    println(a.hashCode())
+    val director = WorkersRepository.findDirector() ?: throwDirectorIsRequired()
+    director.printInfo()
 }
 
-fun method() {}
+// Nothing has no instances. You can use Nothing to represent "a value that never exists":
+// for example, if a function has the return type of Nothing,
+// it means that it never returns (always throws an exception).
+
+// Any -> общий родитель, Nothing -> наследник ВСЕХ классов
+fun throwDirectorIsRequired(): Nothing {
+    throw IllegalStateException("Director is required for this program. Please add him to the file workers.txt")
+}
