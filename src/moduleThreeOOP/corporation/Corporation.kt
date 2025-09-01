ee28a543acee2ad0e4b8432a20a44cf2d642aa62
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-01 16:27:12 +0300

    4.13 Elvis и Unit

--- src/moduleThreeOOP/corporation/Corporation.kt
+++ src/moduleThreeOOP/corporation/Corporation.kt
@@ -3,4 +3,17 @@ package moduleThreeOOP.corporation
 fun main() {
     val director = WorkersRepository.findDirector()
     director?.printInfo()
+    val assistant = WorkersRepository.findAssistant()
+    assistant?.printInfo()
+
+    val directorSalary = director?.salary ?: 0 // если левая часть == null, то = 0
+    val assistantSalary = assistant?.salary ?: 0
+    val sum = assistantSalary + directorSalary
+    println(sum)
+
+    val a: Unit = method() // Unit class -
+    // The type with only one value: the Unit object. This type corresponds to the void type in Java.
+    println(a.hashCode())
 }
+
+fun method() {}
