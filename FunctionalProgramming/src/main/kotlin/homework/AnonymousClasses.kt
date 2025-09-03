acb797c1b721b3852b8ab3c92441bdead79b20b9
Михаил Дюсов <143973@gmail.com>
2025-09-03 21:48:45 +0300

    5.4 Анонимные классы - домашнее задание - выполненное

--- FunctionalProgramming/src/main/kotlin/homework/AnonymousClasses.kt
+++ FunctionalProgramming/src/main/kotlin/homework/AnonymousClasses.kt
@@ -1,5 +1,12 @@
 package homework
 
+fun main() {
+    val clickProcessor = ClickProcessor()
+    setupClickListeners(clickProcessor)
+    val result = clickProcessor.processClick("button1")
+    println(result)
+}
+
 interface ClickListener {
     fun onClick(elementId: String): String
 }
@@ -19,5 +26,22 @@ class ClickProcessor {
 
 // Реализуйте метод setupClickListeners
 fun setupClickListeners(clickProcessor: ClickProcessor) {
-    // TODO: Зарегистрируйте слушателей для "button1", "button2" и "button3" с помощью анонимных классов.
+    // "button1" — возвращает сообщение "Button 1 clicked!".
+    clickProcessor.registerClickListener("button1", object : ClickListener {
+        override fun onClick(elementId: String): String {
+            return "Button 1 clicked!"
+        }
+    })
+    // "button2" — возвращает сообщение "Button 2 clicked!".
+    clickProcessor.registerClickListener("button2", object : ClickListener {
+        override fun onClick(elementId: String): String {
+            return "Button 2 clicked!"
+        }
+    })
+    // "button3" — возвращает сообщение "Button 3 clicked!".
+    clickProcessor.registerClickListener("button3", object : ClickListener {
+        override fun onClick(elementId: String): String {
+            return "Button 3 clicked!"
+        }
+    })
 }
