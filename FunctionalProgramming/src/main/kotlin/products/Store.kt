713b69ab9725911dd349e392eefa5ca80403b672
Михаил Дюсов <143973@gmail.com>
2025-09-04 21:17:58 +0300

    5.5 Лямбда выражения

--- FunctionalProgramming/src/main/kotlin/products/Store.kt
+++ FunctionalProgramming/src/main/kotlin/products/Store.kt
@@ -2,5 +2,20 @@ package products
 
 fun main() {
     val products = ProductRepository.products
-    products.forEach { println(it) }
+
+    var filteredProducts = filter(products) { it.productPrice > 500 }
+    filteredProducts = filter(filteredProducts) { it.productRating > 4 }
+    filteredProducts = filter(filteredProducts) { it.productCategory == ProductCategory.SPORTS }
+
+    filteredProducts.forEach { println(it) }
+}
+
+private fun filter(productCards: List<ProductCard>, isSuitable: (ProductCard) -> Boolean): List<ProductCard> {
+    val result = mutableListOf<ProductCard>()
+    for (productCard in productCards) {
+        if (isSuitable(productCard)) {
+            result.add(productCard)
+        }
+    }
+    return result
 }
--- FunctionalProgramming/src/main/kotlin/profile/Profile.kt
+++ FunctionalProgramming/src/main/kotlin/profile/Profile.kt
@@ -2,5 +2,31 @@ package profile
 
 fun main() {
     val profiles = ProfilesRepository.profiles
-    profiles.forEach { println(it) }
+
+    // создаем функцию и записываем в переменную
+    // если лямбда-выражение принимает один параметр, то компилятор даст ему имя "it" и его можно использовать
+    val olderThan25: (Person) -> Boolean = { it.age > 25 }
+    var filteredProfiles = filter(profiles, olderThan25)
+
+    // но можно сразу передать функцию в метод
+    // если функция - последний параметр, то ее можно записать после закрытых круглых скобок
+    filteredProfiles = filter(filteredProfiles) { it.gender == Gender.MALE }
+
+    filteredProfiles = filter(filteredProfiles) { it.firstName.startsWith("A") }
+
+    filteredProfiles = filter(filteredProfiles) { it.age < 30 }
+
+    filteredProfiles.forEach { println(it) }
+}
+
+// синтаксис функций = "(тип на вход) -> тип на выход"
+// функция высшего порядка = принимает другие функции в качестве параметра или возвращать их
+private fun filter(profiles: List<Person>, isSuitable: (Person) -> Boolean): List<Person> {
+    val result = mutableListOf<Person>()
+    for (profile in profiles) {
+        if (isSuitable(profile)) {
+            result.add(profile)
+        }
+    }
+    return result
 }
