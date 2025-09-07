13a3f7588c0d027505913b03ecbc20bfb8757e16
Михаил Дюсов <143973@gmail.com>
2025-09-07 17:47:18 +0300

    5.13 Map для улучшения производительности - домашнее задание

--- FunctionalProgramming/src/main/kotlin/homework/MapCollection.kt
+++ FunctionalProgramming/src/main/kotlin/homework/MapCollection.kt
@@ -1,5 +1,8 @@
 package homework
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
 /*
     Напишите extension-функцию для коллекции Map, которая будет преобразовывать все значения в коллекции по заданной функции.
 
@@ -22,3 +25,24 @@ fun <K, V, R> Map<K, V>.transformValues(transform: (V) -> R): Map<K, R> {
     }
     return result
 }
+
+@Serializable
+data class UserForMap(
+    @SerialName("id") val id: Int,
+    @SerialName("name") val name: String,
+    @SerialName("email") val email: String,
+    @SerialName("age") val age: Int,
+    @SerialName("is_active") val isActive: Boolean,
+    @SerialName("address") val address: String,
+    @SerialName("phone") val phone: String,
+    @SerialName("created_at") val createdAt: String,
+    @SerialName("updated_at") val updatedAt: String,
+    @SerialName("role") val role: String
+)
+
+/**
+ * Преобразует список пользователей в Map, где ключ — `id`, а значение — объект `User`.
+ * @param users Список пользователей.
+ * @return Коллекция Map пользователей.
+ */
+fun usersToMap(users: List<UserForMap>): Map<Int, UserForMap> = users.associateBy { it.id }
