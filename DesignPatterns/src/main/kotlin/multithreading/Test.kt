02bb26bb7a9ba888fec8f20a6f93ce099cfc208c
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-09 22:45:37 +0300

    Критическая секция, мьютекс и блок синхронизации

--- DesignPatterns/src/main/kotlin/multithreading/Test.kt
+++ DesignPatterns/src/main/kotlin/multithreading/Test.kt
@@ -3,22 +3,36 @@ package multithreading
 import kotlin.concurrent.thread
 
 fun main() {
-    Thread {
-        repeat(100_000) {
-            print(" 0 ")
-        }
-    }.start()
+    val counter = Counter()
 
-    // kotlin.concurrent.thread - не нужно вызывать явно .start()
-    thread {
-        repeat(100_000) {
-            print(" 1 ")
-            // остановить поток на 100 миллисекунд
-            Thread.sleep(100)
+    val thread1 = thread {
+        repeat(1_000_000) {
+            counter.increment()
         }
     }
 
-    repeat(100_000) {
-        print(" * ")
+    val thread2 = thread {
+        repeat(1_000_000) {
+            counter.increment()
+        }
+    }
+
+    thread1.join()
+    thread2.join()
+    println(counter.count)
+}
+
+class Counter {
+    // мьютекс
+    private val lock = Any()
+
+    var count = 0
+
+    fun increment() {
+        // блок синхронизации
+        synchronized(lock) {
+            // критическая секция
+            count++
+        }
     }
 }
