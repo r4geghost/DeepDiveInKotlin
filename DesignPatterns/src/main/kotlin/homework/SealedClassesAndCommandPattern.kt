225c25d2fbaf5d78945f355860bfa26e67123165
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-12 23:11:45 +0300

    6.13 Sealed Interface - домашнее задание - выполненное

--- DesignPatterns/src/main/kotlin/homework/SealedClassesAndCommandPattern.kt
+++ DesignPatterns/src/main/kotlin/homework/SealedClassesAndCommandPattern.kt
@@ -12,6 +12,64 @@ import kotlin.concurrent.thread
     обрабатывая их в порядке поступления.
  */
 
+fun interface Command {
+    fun execute()
+}
+
+interface Invoker<T : Command> {
+    fun executeCommand(command: T)
+}
+
+class RemoteControl : Invoker<DeviceCommand> {
+    private val commands = LinkedBlockingQueue<DeviceCommand>()
+
+    init {
+        thread {
+            while (true) {
+                commands.take().execute()
+            }
+        }
+    }
+
+    override fun executeCommand(command: DeviceCommand) {
+        commands.add(command)
+    }
+}
+
+sealed interface DeviceCommand : Command {
+    data class LightOnCommand(val light: Light) : DeviceCommand {
+        override fun execute() = light.turnOn()
+    }
+
+    data class LightOffCommand(val light: Light) : DeviceCommand {
+        override fun execute() = light.turnOff()
+    }
+
+    data class TVOnCommand(val tv: TV) : DeviceCommand {
+        override fun execute() = tv.turnOn()
+    }
+
+    data class TVOffCommand(val tv: TV) : DeviceCommand {
+        override fun execute() = tv.turnOff()
+    }
+
+    data class TVChangeChannelCommand(val tv: TV, val channel: Int) : DeviceCommand {
+        override fun execute() = tv.changeChannel(channel)
+    }
+
+    data class AirConditionerOnCommand(val ac: AirConditioner) : DeviceCommand {
+        override fun execute() = ac.turnOn()
+    }
+
+    data class AirConditionerOffCommand(val ac: AirConditioner) : DeviceCommand {
+        override fun execute() = ac.turnOff()
+    }
+
+    data class AirConditionerSetTempCommand(val ac: AirConditioner, val temp: Int) : DeviceCommand {
+        override fun execute() = ac.setTemperature(temp)
+    }
+}
+
 class Light {
     fun turnOn() = println("Свет включен")
     fun turnOff() = println("Свет выключен")
@@ -33,15 +91,16 @@ fun runCommandTest() {
     val light = Light()
     val tv = TV()
     val ac = AirConditioner()
+    val remote = RemoteControl()
 
-    light.turnOn()
-    tv.turnOn()
-    tv.changeChannel(5)
-    ac.turnOn()
-    ac.setTemperature(22)
-    light.turnOff()
-    tv.turnOff()
-    ac.turnOff()
+    remote.executeCommand(DeviceCommand.LightOnCommand(light))
+    remote.executeCommand(DeviceCommand.TVOnCommand(tv))
+    remote.executeCommand(DeviceCommand.TVChangeChannelCommand(tv, 5))
+    remote.executeCommand(DeviceCommand.AirConditionerOnCommand(ac))
+    remote.executeCommand(DeviceCommand.AirConditionerSetTempCommand(ac, 22))
+    remote.executeCommand(DeviceCommand.LightOffCommand(light))
+    remote.executeCommand(DeviceCommand.TVOffCommand(tv))
+    remote.executeCommand(DeviceCommand.AirConditionerOffCommand(ac))
 }
 
 fun main() {
