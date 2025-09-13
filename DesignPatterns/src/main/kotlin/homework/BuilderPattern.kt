9f33eccc6488a2f42bab9ee3def8e386ee1ad6ec
Михаил Дюсов <143973@gmail.com>
2025-09-13 13:32:05 +0300

    6.14 Builder - домашнее задание - условие

---
+++ DesignPatterns/src/main/kotlin/homework/BuilderPattern.kt
@@ -0,0 +1,26 @@
+package homework
+
+/*
+    Вам необходимо создать объект класса Product, используя Product.Builder.
+    Конструктор Product скрыт, и создать объект можно только через билдер.
+
+    Создаваемый объект должен соответствовать следующим параметрам:
+        Название: "Smartphone"
+        Цена: 999.99
+        Производитель: "TechCorp"
+        Гарантия: 24
+
+    Для этого используйте следующие методы класса Product.Builder:
+        name(name: String): Builder
+        price(price: Double): Builder
+        manufacturer(manufacturer: String): Builder
+        warranty(months: Int): Builder
+        build(): Product
+    Создайте объект Product, используя данные методы, установите все параметры и передайте его в переменную product.
+ */
+
+fun task() : Product {
+    val product =
+
+    return product
+}
