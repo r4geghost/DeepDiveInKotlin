fdaa964b199c9595a1336a49f04e74113c329f02
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-14 17:57:40 +0300

    Kotlin - профессиональный уровень - коллекции

---
+++ Collections/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "meekie"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
