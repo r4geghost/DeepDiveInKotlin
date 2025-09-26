c0ccc77bac67070f27b68792e0b9accfc39754a2
Михаил Дюсов <mikhail.dyusov@rtl-consulting.ru>
2025-09-26 16:56:55 +0300

    8.21 Знакомство с API

---
+++ FlowAndChannels/src/main/kotlin/dictionary/Repository.kt
@@ -0,0 +1,9 @@
+package dictionary
+
+object Repository {
+
+    private const val BASE_ULR = "https://api.api-ninjas.com/v1/dictionary?word="
+    private const val API_KEY = "PNTI6z/FDA9Z5c6ySdGndg==pLFQ9sI3ftVGyTaa"
+    private const val HEADER_KEY = "X-Api-Key"
+
+}
